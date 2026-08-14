cf45ebe590ced7745ce229f0c9238edfe599d928
Chintan0906 <jainchintan14@gmail.com>
2026-08-14 20:34:44 +0530

    Time: 0 ms (100.00%) | Memory: 59.5 MB (99.17%) - LeetSync

---
+++ 1659-get-the-maximum-score/get-the-maximum-score.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size(),m=nums2.size();
+        int i=0,j=0;
+        long long res=0;
+        long long s1=0,s2=0;
+        while(i<n && j<m){
+            if(nums1[i]<nums2[j]) s1+=nums1[i++];
+            else if(nums1[i]>nums2[j]) s2+=nums2[j++];
+            else{
+                res+=nums1[i]+max(s1,s2);
+                s1=0,s2=0;
+                i++;
+                j++;
+            }
+        }
+        while(i<n) s1+=nums1[i++];
+        while(j<m) s2+=nums2[j++];
+        res+=max(s1,s2);
+        return res%1000000007LL;
+    }
+};
