76de3caf4a7b041c4be92fadfdc5994a15ef122a
Chintan0906 <jainchintan14@gmail.com>
2026-08-15 22:36:45 +0530

    Time: 27 ms (94.93%) | Memory: 85.6 MB (85.88%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        int n=candyType.size();
+        sort(candyType.begin(),candyType.end());
+        int unique=1;
+        for(int i=1;i<n;i++){
+            if(candyType[i]!=candyType[i-1]) unique++;
+        }
+        return min(unique,n/2);
+    }
+};
